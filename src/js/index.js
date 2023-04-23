7355b115dd5747024ea16d3c2ea9b759b6e4ca43
oAndin <112807863+oAndin@users.noreply.github.com>
2023-04-23 14:07:17 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('.button');
 
-const image = document.querySelectorAll('.image');
-const text = document.querySelectorAll('.text');
+// const image = document.querySelectorAll('.image');
+// const text = document.querySelectorAll('.text');
 
 buttons.forEach((button, indice) => {
     button.addEventListener('click', () => {
@@ -9,16 +9,15 @@ buttons.forEach((button, indice) => {
         const buttonSelected = document.querySelector('.selected');
         buttonSelected.classList.remove('selected');
 
-        button.classList.add('selected');
+        // button.classList.add('selected');
        
-        const imageSelected = document.querySelector('.selected');
-        imageSelected.classList.remove('selected');
-        const textSelected = document.querySelector('.selected');
-        textSelected.classList.remove('selected');
+        // const imageSelected = document.querySelector('.selected');
+        // imageSelected.classList.remove('selected');
         
-        text[indice].classList.add('selected');
-        image[indice].classList.add('selected');
+        // const textSelected = document.querySelector('.selected');
+        // textSelected.classList.remove('selected');
         
-
+        // text[indice].classList.add('selected');
+        // image[indice].classList.add('selected');
     })
 })
